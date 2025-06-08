109fb5aa1178105f284d0c436b87b3b81d4d7e40
PHOOSITH\phoos <phoosith.p@ku.th>
2025-06-08 15:48:23 +0700

    แก้ไขไฟล์TodoShow

--- src/components/TodoShow.jsx
+++ src/components/TodoShow.jsx
@@ -1,7 +1,7 @@
 
 const TodoShow = ({todo,setTodo}) => {
   return (
-    <div className="grid grid-cols-3 gap-6 ml-44 mt-6">
+    <div className="grid grid-cols-3 gap-6 ml-44 mt-6 overflow-y-auto">
             {
               todo.map((t,index)=>(
                 
@@ -12,7 +12,7 @@ const TodoShow = ({todo,setTodo}) => {
                   <button
 
                   onClick={()=>setTodo(todo.filter((item)=>item.id !== t.id))}
-                  type="button" className='fixed ml-2 pl-2 font-semibold rounded-md h-8 pr-4 cursor-pointer bg-white transition ease-in duration:400 hover:bg-black hover:text-white'>
+                  type="button" className='inline-block ml-2 pl-2 font-semibold rounded-md h-8 pr-4 cursor-pointer bg-white transition ease-in duration:400 hover:bg-black hover:text-white'>
                     Delete</button>
 
                 </div>
